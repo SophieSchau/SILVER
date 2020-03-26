87b32b8b9f0a40b86c672fc21e57b63adce7f9cd
Sophie <sophie.schauman@dtc.ox.ac.uk>
2020-03-26 15:03:44 +0000

    example 3 finalised

--- examples/example3_ep_efficiency/example3_ep_efficiency.m
+++ examples/example3_ep_efficiency/example3_ep_efficiency.m
@@ -1,10 +1,61 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%   Does SILVER perform better than the golden ratio when the set of window
-%   sizes to optimize for are discrete temporal resolutions?
+%   How does efficiency, as calculated by the electrostatic potential, vary
+%   within a range of window sizes or for multiple temporal resolutions for
+%   SILVER and for the golden ratio method?
 %                                              
 %   Sophie Schauman 2020                       
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 clear
 close all
+
+%% 1. Choose  sets of window sizes, S, to consider
+
+S1 = [16,32,48];
+S2 = [32:39];
+S = {S1,S2};
+
+%% 2. Do SILVER optimization for those ranges
+for s = S
+   savename = ['examples/precalculated/silver_' strrep(num2str(s{:}),' ', '_') '.mat'];
+   if ~exist(savename, 'file')
+        SILVER_2D(s{:},'electrostatic_potential',savename) ;
+   end
+   disp([num2str(s{:}) ' done'])
+end
+
+
+%% 3. Vizualise results
+
+
+eff_GR = efficiency_range(gr2D,2:50,'electrostatic_potential');
+
+figure(3)
+plot(2:50,eff_GR,'-o','Linewidth', 3, 'markersize', 10)
+hold on
+
+lg{1} = 'Golden ratio';
+n = 1;
+for s = S
+    n = n+1;
+    load(['examples/precalculated/silver_' strrep(num2str(s{:}),' ', '_') '.mat'],'eff_SILVER', 'ratio')
+    eff_SILVER_all = efficiency_range(ratio,2:50,'electrostatic_potential');
+    h = plot(2:50,eff_SILVER_all,'x-','Linewidth', 3, 'markersize', 5);
+    c = get(h,'Color');
+    h.Color(4) = 0.2;
+    plot(s{:},eff_SILVER,'o','Linewidth', 3, 'markersize', 10,'color', c)
+    lg{n} = ['SILVER: S = \{' num2str(s{:}) '\} full range'];
+    n = n+1;
+    lg{n} = ['SILVER: S = \{' num2str(s{:}) '\} in optimized region'];
+end
+
+set(gca,'FontSize',18)
+set(gcf,'Position',[124 359 876 439])
+legend(lg{:}, 'location', 'southoutside')
+xlabel('Window size')
+ylabel('Efficiency')
+axis([0,52,0.9,1])
+
+savefig('examples/example3_ep_efficiency/example3_ep_efficiency_result.fig')
+saveas(gcf,'examples/example3_ep_efficiency/example3_ep_efficiency_result.tiff')
 
---
+++ examples/example4_psf/example4_psf.m
@@ -0,0 +1,12 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   What do the point-spread-functions of trajectories optimized with
+%   SILVER look like compared with golden ratio method or uniform radial
+%   sampling?
+%                                              
+%   Sophie Schauman 2020                       
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+clear
+close all
+
+
