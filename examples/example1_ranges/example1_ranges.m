dbb8ac5f945c7ab065b56dea5b276b0440b5b130
Sophie <sophie.schauman@dtc.ox.ac.uk>
2020-04-10 14:01:27 +0100

    new vizualisation of efficiency increse in range

--- examples/example1_ranges/example1_ranges.m
+++ examples/example1_ranges/example1_ranges.m
@@ -18,7 +18,7 @@ for m = M
     for l = L
        savename = ['examples/precalculated/silver_' num2str(m) 'to' num2str(m+l) '.mat'];
        if ~exist(savename, 'file')
-            SILVER_2D(m:m+l,'electrostatic_potential',savename) ;
+            SILVER_2D(m:m+l,'electrostatic_potential', savename) ;
        end
        disp([num2str(m) 'to' num2str(m+l) ' done'])
     end
@@ -32,31 +32,46 @@ if ~exist(savename, 'file')
     for w = 1:length(M)
         m = M(w);
         for l = L
-            load(['examples/precalculated/silver_' num2str(m) 'to' num2str(m+l) '.mat'],'eff_SILVER')
-            prcnt_increase(w,l) = (min(eff_SILVER)/min(efficiency_range(gr2D,m:m+l,'electrostatic_potential'))-1)*100;
+            load(['examples/precalculated/silver_' num2str(m) 'to' num2str(m+l) '.mat'],'eff_SILVER','ratio')
+            min_eff_SILVER(w,l) = (min(eff_SILVER));
+            min_eff_GR(w,l) = min(efficiency_range(gr2D,m:m+l,'electrostatic_potential'));
+            prcnt_increase(w,l) = ((min_eff_SILVER(w,l)/min_eff_GR(w,l))-1)*100;
+            SILVER_ratio(w,l) = ratio;
 
         end
     end
-    save(savename, 'prcnt_increase','M', 'L')
+    save(savename, 'prcnt_increase','SILVER_ratio','min_eff_GR', 'min_eff_SILVER','M', 'L')
 else
     warning('using pre-calculated results')
     load(savename);
 end
 
 %% 4. Vizualise result
+tiny_ratios = [];
+tiny_ratios_higher_order = [];
+for N = 1
+    tiny_ratios = cat(1,tiny_ratios,gen_gr2D(N,1));
+    tiny_ratios = cat(1,1-tiny_ratios,gen_gr2D(N,1));
+end
 
 figure(1)
-plot(L+1,prcnt_increase(1,:), 'x-','Linewidth', 3, 'markersize', 10)
+c_map = get(gca,'colororder');
+plot(L,min_eff_SILVER(1,:), 'o-','Linewidth', 3, 'markersize', 5, 'Color',c_map(1,:))
 hold on
-plot(L+1,prcnt_increase(2,:),'x-','Linewidth', 3,'markersize', 10)
-plot(L+1,prcnt_increase(3,:),'x-', 'Linewidth', 3,'markersize', 10)
+plot(L,min_eff_GR(1,:), '--','Linewidth', 2, 'markersize', 5, 'Color',[c_map(1,:), 0.5])
+plot(L,min_eff_SILVER(2,:),'o-','Linewidth', 3,'markersize', 5, 'Color',c_map(2,:))
+plot(L,min_eff_GR(2,:), '--','Linewidth', 2, 'markersize', 5, 'Color',[c_map(2,:), 0.5])
+plot(L,min_eff_SILVER(3,:),'o-', 'Linewidth', 3,'markersize', 5, 'Color',c_map(3,:))
+plot(L,min_eff_GR(3,:), '--','Linewidth', 2, 'markersize', 5, 'Color',[c_map(3,:), 0.5])
 
 set(gca,'FontSize',18)
 set(gcf,'Position',[124 359 876 439])
-axis([0,max(L)+1,0,5])
-legend('Minimum window size = 4', 'Minimum window size = 16', 'Minimum window size = 32', 'location', 'northeast')
-xlabel('Range width')
-ylabel('SILVER efficiency increase, %')
+axis([0,max(L)+1,ylim])
+legend('SILVER, M = 4', 'GR, M = 4', 'SILVER, M = 16', 'GR, M = 16', 'SILVER, M = 32', 'GR, M = 32','location', 'northeast')
+xlabel('Range width, R')
+ylabel('Minimum efficiency, \eta')
+
 
 savefig('examples/example1_ranges/example1_ranges_result.fig')
 saveas(gcf,'examples/example1_ranges/example1_ranges_result.tiff')
+
---
+++ support_functions/gen_gr2D.m
@@ -0,0 +1,4 @@
+function [ratio] = gen_gr2D(N,M)
+    ratio = 1/(N+1/(M-1+1/gr2D));
+end
+
