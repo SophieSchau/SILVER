a347e66b85d0c98b6c65b423a0eb21899ff11b51
Sophie <sophie.schauman@dtc.ox.ac.uk>
2020-06-22 16:50:11 +0100

    gitignore including gifs

--- setup_SILVER.m
+++ setup_SILVER.m
@@ -4,7 +4,16 @@ gunzip(url, 'SILVER_data');
 untar('SILVER_data/SILVER_data.tar','SILVER_data');
 
 %% Example 1 data
+system('cp SILVER_data/example1/* examples/example1_ranges/.');
 
+%% Example 2 data
+system('cp SILVER_data/example2/* examples/example2_multiple_tempres/.');
 
+%% Example 3 data
+system('cp SILVER_data/example3/* examples/example3_ep_efficiency/.');
 
+%% Example 4 data
+system('cp SILVER_data/example4/* examples/example4_psf/.');
 
+%% Example 5 data
+system('cp -r SILVER_data/example5/* examples/example5_gfactor/.');
