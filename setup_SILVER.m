4e1d84f7257e2fcba287e054a2a76d5206bfba3e
Sophie <sophie.schauman@dtc.ox.ac.uk>
2020-06-23 17:27:24 +0100

    separate in-vivo and other data

--- setup_SILVER.m
+++ setup_SILVER.m
@@ -1,36 +1,41 @@
-%% Download data
-system('tar -zxvf ./SILVER_data.tar.gz')
+%% Download data (basic)
+system('tar -zxvf ./SILVER_data_basic.tar.gz');
 
 %% Example 1 data
-system('cp SILVER_data/example1/* examples/example1_ranges/.');
-system('rm SILVER_data/example1/*');
+system('cp SILVER_data_basic/example1/* examples/example1_ranges/.');
+system('rm SILVER_data_basic/example1/*');
 
 %% Example 2 data
-system('cp SILVER_data/example2/* examples/example2_multiple_tempres/.');
-system('rm SILVER_data/example2/*');
+system('cp SILVER_data_basic/example2/* examples/example2_multiple_tempres/.');
+system('rm SILVER_data_basic/example2/*');
 
 %% Example 3 data
-system('cp SILVER_data/example3/* examples/example3_ep_efficiency/.');
-system('rm SILVER_data/example3/*');
+system('cp SILVER_data_basic/example3/* examples/example3_ep_efficiency/.');
+system('rm SILVER_data_basic/example3/*');
 
 %% Example 4 data
-system('cp SILVER_data/example4/* examples/example4_psf/.');
-system('rm SILVER_data/example4/*');
+system('cp SILVER_data_basic/example4/* examples/example4_psf/.');
+system('rm SILVER_data_basic/example4/*');
 
 %% Example 5 data
-system('cp -r SILVER_data/example5/* examples/example5_gfactor/.');
-system('rm -r SILVER_data/example5/*');
+system('cp -r SILVER_data_basic/example5/* examples/example5_gfactor/.');
+system('rm -r SILVER_data_basic/example5/*');
 
 %% Example 6 data
-system('cp SILVER_data/example6/* examples/example6_phantom_simu/.');
-system('rm SILVER_data/example6/*');
-% 
-%% Example 7 data
-system('cp -r SILVER_data/example7/* examples/example7_invivo/.');
-system('rm -r SILVER_data/example7/*');
+system('cp SILVER_data_basic/example6/* examples/example6_phantom_simu/.');
+system('rm SILVER_data_basic/example6/*');
 
 %% Precalculated SILVER optimizations
-system('cp SILVER_data/precalculated/* examples/precalculated/.');
+system('cp SILVER_data_basic/precalculated/* examples/precalculated/.');
 
-%% Final cleanup
-system('rm -r SILVER_data')
+%% Cleanup
+system('rm -r SILVER_data_basic');
+
+
+%% Example 7 (in-vivo) data
+
+system('tar -zxvf ./SILVER_data_invivo.tar.gz');
+system('cp -r SILVER_data_invivo/example7/* examples/example7_invivo/.');
+system('rm -r SILVER_data_invivo');
+
+
